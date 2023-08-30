4e53885a7be17c09168bb81cb436ecf690371eae
mukhagaliaryn <mukhagaliaryn@gmail.com>
2023-08-30 12:03:24 +0600

    Done

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='main'),
+    path('payment/', views.payment, name='payment'),
+    path('article/', views.article, name='article'),
+    path('publishers/', views.publishers, name='publishers'),
+    path('about/', views.about, name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 # Main page
 def index(request):
     return render(request, 'main/index.html', {})
+
+
+# payment page
+def payment(request):
+    return render(request, 'main/payment.html', {})
+
+
+# article page
+def article(request):
+    return render(request, 'main/article.html', {})
+
+
+# publishers page
+def publishers(request):
+    return render(request, 'main/publishers.html', {})
+
+
+# about page
+def about(request):
+    return render(request, 'main/about.html', {})
